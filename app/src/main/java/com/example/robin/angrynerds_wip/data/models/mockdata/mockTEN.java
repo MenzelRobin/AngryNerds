c38a708cdd71c880f9ad28a03091fb6b4850c587
Ruthild <42970488+Ruthild@users.noreply.github.com>
2018-10-27 22:10:07 +0200

    first draft getter and setter and mockdata folder

---
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/models/mockdata/mockTEN.java
@@ -0,0 +1,10 @@
+package com.example.robin.angrynerds_wip.data.models.mockdata;
+
+import com.example.robin.angrynerds_wip.data.models.tens.TEN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mockTEN {
+    public static ArrayList<TEN> mockTENs = new ArrayList<TEN>();
+}
