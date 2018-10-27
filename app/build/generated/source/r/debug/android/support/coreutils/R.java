04e6d1d3e9e14c47ce86b57945f404fc3276f59d
MenzelRobin <robin.menzel@ymail.com>
2018-10-27 15:11:34 +0200

    Layout Improvements

--- app/build/generated/source/r/debug/android/support/coreutils/R.java
+++ app/build/generated/source/r/debug/android/support/coreutils/R.java
@@ -95,12 +95,12 @@ public final class R {
         public static final int status_bar_notification_info_maxnum = 0x7f080004;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f09001c;
-        public static final int notification_action_tombstone = 0x7f09001d;
-        public static final int notification_template_custom_big = 0x7f09001e;
-        public static final int notification_template_icon_group = 0x7f09001f;
-        public static final int notification_template_part_chronometer = 0x7f090020;
-        public static final int notification_template_part_time = 0x7f090021;
+        public static final int notification_action = 0x7f09001d;
+        public static final int notification_action_tombstone = 0x7f09001e;
+        public static final int notification_template_custom_big = 0x7f09001f;
+        public static final int notification_template_icon_group = 0x7f090020;
+        public static final int notification_template_part_chronometer = 0x7f090021;
+        public static final int notification_template_part_time = 0x7f090022;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0b001f;
