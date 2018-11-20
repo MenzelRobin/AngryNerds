e5d4ccf6964a6769d6ac60e0529efcde6ce9ec65
JoschNa <44116079+JoschNa@users.noreply.github.com>
2018-11-20 21:15:09 +0100

    Update for merge

--- app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/MockData.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/MockData.java
@@ -1,7 +1,7 @@
 package com.example.robin.angrynerds_wip.data.models.utils;
 
 import android.graphics.Color;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 
 import com.example.robin.angrynerds_wip.data.models.tens.Event;
 import com.example.robin.angrynerds_wip.data.models.tens.Note;
