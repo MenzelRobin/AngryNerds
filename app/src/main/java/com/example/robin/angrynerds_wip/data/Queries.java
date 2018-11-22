3c1ad12baeca1390d0a2f17db4d08b7fa375127f
Ruthild <42970488+Ruthild@users.noreply.github.com>
2018-11-22 13:26:54 +0100

    Queries Klasse mit leeren Methoden für Jan (id zu String)

--- app/src/main/java/com/example/robin/angrynerds_wip/data/Queries.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/Queries.java
@@ -8,7 +8,7 @@ import com.example.robin.angrynerds_wip.data.models.tens.Todo;
 public class Queries {
 
     //TODO Jan: jeweiliges Objekt mit übergebener ID zurückgeben (wenn kein Objekt mit ID dann return null)
-    public static TEN getByID(int id) {
+    public static TEN getByID(String id) {
         return null;
     }
 
@@ -18,7 +18,7 @@ public class Queries {
     }
 
     //TODO Jan: vorhanden Eintrag (mit übergebener ID) mit den Werten aus dem übergebenen TEN überschreiben
-    public static void updateTEN(int id, TEN ten) {
+    public static void updateTEN(String id, TEN ten) {
     }
 
     //TODO Jan: alle vorhandenen TENs in einem Array übergeben
