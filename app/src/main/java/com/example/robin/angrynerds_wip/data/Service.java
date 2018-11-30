11d1658e6e0244373852023eb8be5eaf0dd0bfa4
Ruthild <rg008b@gmail.com>
2018-11-30 16:13:30 +0100

    getAllTENs mit MockDaten

--- app/src/main/java/com/example/robin/angrynerds_wip/data/Service.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/Service.java
@@ -4,8 +4,10 @@ import com.example.robin.angrynerds_wip.data.models.tens.Event;
 import com.example.robin.angrynerds_wip.data.models.tens.Note;
 import com.example.robin.angrynerds_wip.data.models.tens.TEN;
 import com.example.robin.angrynerds_wip.data.models.tens.Todo;
+import com.example.robin.angrynerds_wip.data.models.utils.MockData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /*
@@ -14,9 +16,9 @@ import java.util.ArrayList;
  */
 public class Service {
 
-    /*
+    /*--------------------------------------------------
         Methods for saving one TEN object
-     */
+     --------------------------------------------------*/
     public static void saveTEN(TEN newTEN) {
         TEN oldTEN = Queries.getByID(newTEN.getID());
         if (oldTEN == null) { //new TEN (not in database)
@@ -26,10 +28,16 @@ public class Service {
         }
     }
 
-    /*
+    /*--------------------------------------------------
         Method to get all TEN objects in an arraylist
-     */
+     --------------------------------------------------*/
     public static ArrayList<TEN> getAllTENs() {
+        //MOCKDATA
+        return MockData.tenMockData;
+    }
+    /*
+    public static ArrayList<TEN> getAllTENs() {
+
         //TODO Ruthild: Sortieren
         ArrayList<TEN> allTEN = new ArrayList<TEN>();
         Todo[] allTodos = Queries.getAllTodods();
@@ -48,5 +56,23 @@ public class Service {
             allTEN.add(n);
         }
         return allTEN;
+    } */
+
+    /*--------------------------------------------------
+        Methods to get one TEN object by ID
+     --------------------------------------------------*/
+    public static Todo getTodoByID(String id) {
+        Todo todo = Queries.getTodoByID(id);
+        return todo;
+    }
+
+    public static Event getEventByID(String id) {
+        Event event = Queries.getEventByID(id);
+        return event;
+    }
+
+    public static Note getNoteByEvent(String id) {
+        Note note = Queries.getNoteByID(id);
+        return note;
     }
 }
